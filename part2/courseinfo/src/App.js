642435b5b536d0fe8859c0dbd6c36d227a73fe5a
jjonah <jjonah@veracode.com>
2022-12-23 17:48:06 -0500

    2.1: Course information step6

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,81 @@
+
+const App = () => {
+  // const-definitions
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+      <div>
+        <Course course={course} />
+      </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+      <div>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        {/*<Total parts={course.parts}/>*/}
+      </div>
+  )
+}
+
+const Header = (props) => {
+  console.log(props)
+  return (
+      <div>
+        <h1>{props.course}</h1>
+      </div>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+      <div>
+        {parts.map(part =>
+            <Part key={part.id} part={part.name} exercises={part.exercises}/>
+        )}
+
+      </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+      <div>
+      <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+      </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+  <div>
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  </div>
+  )
+}
+
+export default App
